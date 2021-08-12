ef2e4671b2baada43a7c8d46f0d1024adcd55947
Michael Rider <mikepdxrider@gmail.com>
2021-08-12 15:10:02 -0700

    Added html structure and built unit tests.

--- test/example.test.js
+++ test/example.test.js
@@ -1,18 +1,49 @@
 // IMPORT MODULES under test here:
-// import { example } from '../example.js';
+import { add } from '../app.js';
+import { subtract } from '../app.js';
+import { multiply } from '../app.js';
+import { divide } from '../app.js';
 
 const test = QUnit.test;
 
-test('time to test a function', (expect) => {
-    //Arrange
-    // Set up your arguments and expectations
-    const expected = true;
-    
-    //Act 
-    // Call the function you're testing and set the result to a const
-    const actual = true;
+// Addition
+test('Should take in 5 and 5 and return 10', (expect) => {
 
-    //Expect
-    // Make assertions about what is expected versus the actual result
+    const expected = 10;
+   
+    const actual = add(5, 5);
+    
     expect.equal(actual, expected);
 });
+
+// Subtraction
+test('Should take in 15 and 5 and return 10', (expect) => {
+
+    const expected = 10;
+   
+    const actual = subtract(15, 5);
+    
+    expect.equal(actual, expected);
+});
+
+// Multiplication
+test('Should take in 2 and 5 and return 10', (expect) => {
+
+    const expected = 10;
+   
+    const actual = multiply(2, 5);
+    
+    expect.equal(actual, expected);
+});
+
+// Division
+test('Should take in 20 and 2 and return 10', (expect) => {
+
+    const expected = 10;
+    
+    const actual = divide(20, 2);
+    
+    expect.equal(actual, expected);
+});
+
+
